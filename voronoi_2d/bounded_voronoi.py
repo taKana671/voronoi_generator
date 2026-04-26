143a8ace6e16a95dd17e1fd8437486fc33fbb120
taKana671 <kanae5321@gmail.com>
2026-04-26 14:28:58 +0900

    Added comments.

--- voronoi_2d/bounded_voronoi.py
+++ voronoi_2d/bounded_voronoi.py
@@ -4,6 +4,16 @@ from shapely.geometry import Polygon
 
 
 class BoundedVoronoiGenerator:
+    """A class that generates vertex coordinates of Voronoi cells clipped to a square
+
+        Args:
+            pts (numpy.ndarray): coordinates of points to construct a Voronoi diagram from; default is None.
+                If `None` is specified, a number of points equal to `cut_points` will be generated randomly.
+            bnd (numpy.ndarray): clipping boundary; default is None.
+                If `None` is specified, a square with a side length of 1 is used.
+            cut_points (int): number of Voronoi cells; default is 10.
+            shrink (float): specify how much to shrink the Voronoi cells.
+    """
 
     def __init__(self, pts=None, bnd=None, cnt_points=10, shrink=0.02):
         self.shrink = shrink
@@ -37,13 +47,20 @@ class BoundedVoronoiGenerator:
 
 
 class ConvexPolygonGenerator:
-    """A class to partition a general convex polygon, other than a square, into a Voronoi cell
+    """A class that randomly generates vertex coordinates inside a polygon
+
+        Args:
+            bnd (numpy.ndarray): coordinates of the vertices of a polygon
     """
 
     def __init__(self, bnd):
         self.bnd = bnd
 
     def get_cnt_points_from_area(self, bnd_hull):
+        """Determine the number of Voronoi cells based on the area of the polygon.
+            Args:
+                bnd_hull (scipy.spatial.ConvexHull)
+        """
         if (area := bnd_hull.volume) < 0.02:
             return 2
         elif area < 0.03:
@@ -64,14 +81,19 @@ class ConvexPolygonGenerator:
             return 10
 
     def __iter__(self):
+        """Determine a number based on the area of the polygon, and then randomly generate
+           the vertex coordinates of that number within the polygon.
+        """
         bnd_hull = ConvexHull(self.bnd)
         cnt_pts = self.get_cnt_points_from_area(bnd_hull)
 
+        # A matrix representing the boundaries of a domain
         bnd_tmp = bnd_hull.equations
         bnd_mat = np.matrix(bnd_tmp)
         a_bnd = np.array(bnd_mat[:, 0:2])
         b_bnd = np.array(bnd_mat[:, 2])
 
+        # The rectangle surrounding the area
         xmin = np.min(self.bnd[:, 0])
         xmax = np.max(self.bnd[:, 0])
         ymin = np.min(self.bnd[:, 1])
@@ -85,6 +107,7 @@ class ConvexPolygonGenerator:
             pt[1] = ymin + (ymax - ymin) * pt[1]
             n = len(self.bnd)
 
+            # Determine whether a point lies inside the polygon
             if (np.round(np.dot(a_bnd, pt.transpose()), n) <= np.round(-b_bnd.transpose(), n)).all():
                 yield pt
                 i += 1
