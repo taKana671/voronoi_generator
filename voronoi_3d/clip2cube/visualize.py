b56d4b363ccb189ab561dd07bd1b37b5efc1f5e6
taKana671 <kanae5321@gmail.com>
2026-05-17 11:48:40 +0900

    Improved the aspect ratio.

--- voronoi_3d/clip2cube/visualize.py
+++ voronoi_3d/clip2cube/visualize.py
@@ -5,18 +5,20 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from .clip2cube import VoronoiClip2Cube
 
 
-def visualize(cut_points=10, cube_size=1., diff=.5, alpha=.6):
+def visualize(cut_points=30, cube_size=1., diff=.5, alpha=.6):
     polyhedrons = [polyhedron for polyhedron in VoronoiClip2Cube(cut_points, cube_size, diff)]
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    ax.set_aspect('equal', adjustable='box')
     offset = np.full(3, cube_size / 2)
+    rng = np.random.default_rng()
 
     for polyhedron in polyhedrons:
         polygon = Poly3DCollection(
             [poly + offset for poly in polyhedron],
             alpha=alpha,
-            facecolors=np.random.uniform(0, 1, 3),
+            facecolors=rng.uniform(0, 1, 3),
             linewidth=0.8,
             edgecolors='gray'
         )
